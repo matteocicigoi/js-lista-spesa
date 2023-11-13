ff10b3095e484ce2766de33850f526c58884b961
matteocicigoi <matteo.cicigoi1@hotmail.it>
2023-11-13 14:16:00 +0100

    feat: lista

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 'use srtict';
+
+// elementi della spesa
+const elementi = ['Pane', 'Acqua', 'Pasta'];
+
+// ul 
+const lista = document.querySelector('.lista');
+
+//contatore
+let contatore = 0;
+
+while(contatore < elementi.length){
+    const elemento = document.createElement('li');
+    elemento.append(elementi[contatore]);
+    lista.append(elemento);
+    contatore++;
+}
